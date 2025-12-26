3faee96775e38e6eddb3c99ca09de55f8dc79504
codesalotl <jaspertoribio10@gmail.com>
2025-12-26 16:46:20 +0800

    Updated code

--- resources/js/pages/lgu-profile/lgu-profile-layout.tsx
+++ resources/js/pages/lgu-profile/lgu-profile-layout.tsx
@@ -12,6 +12,15 @@ import {
     FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -74,7 +83,33 @@ export default function LguProfileLayout() {
                             <FormItem>
                                 <FormLabel>Username</FormLabel>
                                 <FormControl>
-                                    <Input placeholder="shadcn" {...field} />
+                                    <Select>
+                                        <SelectTrigger className="w-[180px]">
+                                            <SelectValue placeholder="Select a fruit" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectLabel>
+                                                    Fruits
+                                                </SelectLabel>
+                                                <SelectItem value="apple">
+                                                    Apple
+                                                </SelectItem>
+                                                <SelectItem value="banana">
+                                                    Banana
+                                                </SelectItem>
+                                                <SelectItem value="blueberry">
+                                                    Blueberry
+                                                </SelectItem>
+                                                <SelectItem value="grapes">
+                                                    Grapes
+                                                </SelectItem>
+                                                <SelectItem value="pineapple">
+                                                    Pineapple
+                                                </SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
                                 </FormControl>
                                 <FormDescription>
                                     This is your public display name.
