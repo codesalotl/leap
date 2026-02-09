4c84af647e44d0c957803b93c7e0830e673d6712
codesalotl <jaspertoribio10@gmail.com>
2026-02-09 15:52:49 +0800

    comment "expand"

--- resources/js/pages/aip/aip-summary-table/data-table.tsx
+++ resources/js/pages/aip/aip-summary-table/data-table.tsx
@@ -97,7 +97,7 @@ export default function DataTable<TData, TValue>({
         state: {
             sorting,
             columnFilters,
-            expanded,
+            // expanded,
         },
         onSortingChange: setSorting,
         onColumnFiltersChange: setColumnFilters,
